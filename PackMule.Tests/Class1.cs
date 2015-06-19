56ceeb965e8c3f3bc3f66032c6a735d1b272b820
Xcelled <j12@ketelaar.com>
2015-06-18 23:19:52 -0400

    Added test project

---
+++ PackMule.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackMule.Tests
+{
+    public class Class1
+    {
+    }
+}
